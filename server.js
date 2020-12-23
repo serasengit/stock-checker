79a39a10a09f57b88bb75795adab717b22a9ad1d
sasensio <Sergio.Asensio@ust-global.com>
2020-12-23 05:51:33 +0100

    Stock WebSite Service completed

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const cron = require("node-cron");
 const express = require("express");
+const websiteService = require("./services/website.service.js");
 
 // Initialize the app
-let app = express();
+const app = express();
 // Setup server port
 const port = process.env.PORT || 4201;
 // Launch app to listen to specified port
@@ -15,6 +16,10 @@ app.listen(port, function () {
 app.use(express.static("./dist/stock-checker"));
 
 // Schedule tasks to be run on the server.
-cron.schedule("* * * * *", function () {
+cron.schedule("*/60 * * * * *", function () {
   console.log("running a task every minute");
+  const mail = websiteService.getPage(
+    process.env.STOCK_URL ||
+      "https://www.pccomponentes.com/buscar/?query=GeForce+RTX+3080"
+  );
 });
---
+++ services/website.service.js
@@ -0,0 +1,49 @@
+const request = require("request");
+const cheerio = require("cheerio");
+const fs = require("fs");
+const axios = require("axios").default;
+
+exports.getPage = async function (url) {
+  try {
+    const res = await axios.get(url);
+    const availableProducts = getAvailableProducts(res.data);
+    console.log(
+      !availableProducts.length > 0
+        ? "No hay productos disponibles" + availableProducts
+        : "Los productos disponibles son: " + availableProducts
+    );
+  } catch {
+    console.error(
+      `ERROR: An error occurred while trying to fetch the URL: ${url}`
+    );
+  }
+};
+
+function getAvailableProducts(html) {
+  const $ = cheerio.load(html);
+  const availableProducts = [];
+  $(".c-product-card__content").each((index, product) => {
+    let title;
+    let isAvailable = false;
+    let url = "https://www.pccomponentes.com";
+    let price;
+    product.children.forEach((productInfo, index) => {
+      if (productInfo.attribs.class === "c-product-card__header") {
+        title = productInfo.lastChild.lastChild.lastChild.data;
+        url += productInfo.lastChild.lastChild.attribs.href;
+      }
+      if (
+        productInfo.attribs.class.includes("c-product-card__availability") &&
+        productInfo.attribs.class.includes("disponibilidad-inmediata")
+      )
+        isAvailable = true;
+      if (
+        productInfo.attribs.class === "c-product-card__prices cy-product-price"
+      )
+        price = productInfo.lastChild.lastChild.lastChild.data;
+    });
+    if (isAvailable) availableProducts.push({ title, price, isAvailable, url });
+  });
+
+  return availableProducts;
+}
